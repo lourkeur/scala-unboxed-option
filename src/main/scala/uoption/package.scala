6564dab4d950ef6e2071f5b109272054a0557895
Louis Bettens <louis@bettens.info>
2020-05-26 12:05:40 +0200

    implement more ops

--- src/main/scala/uoption/package.scala
+++ src/main/scala/uoption/package.scala
@@ -5,8 +5,27 @@ val UNone: UOption[Nothing] = internals.UNone
 object USome:
   def unapply[A](oa: UOption[A]): Option[A] =
     fold[A, Option[A]](oa)(a => Some(a))(None: Option[A])
-  def apply[A](a: A): UOption[A] = wrap[A](a)
+  def apply[A](a: A): UOption[A] = wrap(a)
+
+object UOption:
+  def apply[A](a: A | Null) = if a == null then UNone else a.wrap
 
 extension UOptionOps on [A](self: UOption[A]):
   def isDefined = self.fold(_ => true)(false)
   def isEmpty = !isDefined
+
+  def get = self.fold(a => a)(throw NoSuchElementException("UNone.get"))
+
+extension UOptionExtractOps on [A, B >: A](self: UOption[A]):
+  def getOrElse(default: => B): B = fold[A, B](self)(a => a: B)(default)
+  def toArray(using reflect.ClassTag[B]): Array[B] = fold[A, Array[B]](self)(a => Array(a))(Array.empty)
+
+extension UOptionFlattenOps on [A <: UOption[B], B](self: UOption[A]):
+  def flatten: UOption[B] = fold[A, UOption[B]](self)(a => a)(UNone)
+
+extension UOptionNullOps on [A, B >: A | Null](self: UOption[A]):
+  def orNull: B = fold[A, B](self)(a => a: B)(null)
+
+extension UOptioMapOps on [A, B](self: UOption[A]):
+  def map(f: A => B): UOption[B] = fold(self)(a => wrap(f(a)))(UNone)
+  def flatMap(f: A => UOption[B]): UOption[B] = fold(self)(a => f(a))(UNone)
