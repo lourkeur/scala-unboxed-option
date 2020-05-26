3537f29d0ef025439fddb3f9aab03ad11d647820
Louis Bettens <louis@bettens.info>
2020-05-26 21:12:16 +0200

    add serializable laws

---
+++ src/test/scala/uoption/SerializableTests.scala
@@ -0,0 +1,16 @@
+package uoption
+
+import org.scalacheck._
+import Prop.forAll
+
+import cats.kernel.Eq
+import cats.kernel.laws.SerializableLaws
+import org.typelevel.discipline.Laws
+
+object SerializableTests extends Laws:
+  import SerializableLaws.serializable
+
+  def apply[A: Arbitrary] =
+    new SimpleRuleSet("serialization",
+      "errorless" -> forAll(serializable[A]),
+    )
--- src/test/scala/uoption/UOptionSuite.scala
+++ src/test/scala/uoption/UOptionSuite.scala
@@ -1,7 +1,15 @@
 package uoption
 
-import org.scalacheck.Properties
+import org.scalacheck._
 import cats.kernel.instances.all.{given cats.kernel.Eq[?]}
+
+given [A: Arbitrary] as Arbitrary[UOption[A]] = Arbitrary {
+  Arbitrary.arbitrary[Option[A]].map {
+    case Some(x) => USome(x)
+    case None => UNone
+  }
+}
 
 object UOptionSuite extends Properties("uoption"):
   include(impl.ImplTest[Int, Int].all)
+  include(SerializableTests[UOption[UOption[UOption[UOption[Int]]]]].all)
