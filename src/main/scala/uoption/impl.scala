7b8ceb8089a0afd171d45a7587f7d5c0b7d2fc5f
Louis Bettens <louis@bettens.info>
2020-05-26 18:08:10 +0200

    update WrappedUNone.toString

--- src/main/scala/uoption/impl.scala
+++ src/main/scala/uoption/impl.scala
@@ -4,6 +4,7 @@ opaque type UOption[+A] >: UNone.type = A | UNone.type | WrappedUNone
 case object UNone
 case class WrappedUNone(level: Int):
   assert(level > 0)
+  override def toString = "USome(" * level + UNone + ")" * level
 
 def [A](a: A).wrap: UOption[A] = a match
   case UNone => WrappedUNone(1)
