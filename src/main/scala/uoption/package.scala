2a07eae868d8221b4d4d3e3646bf0343de4a7863
Louis Bettens <louis@bettens.info>
2020-05-26 14:04:19 +0200

    put extension methods in companion

--- src/main/scala/uoption/package.scala
+++ src/main/scala/uoption/package.scala
@@ -10,22 +10,22 @@ object USome:
 object UOption:
   def apply[A](a: A | Null): UOption[A] = if a == null then UNone else wrap(a)
 
-extension UOptionOps on [A](self: UOption[A]):
-  def isDefined = self.fold(false)(_ => true)
-  def isEmpty = !isDefined
+  extension UOptionOps on [A](self: UOption[A]):
+    def isDefined = self.fold(false)(_ => true)
+    def isEmpty = !isDefined
 
-  def get = self.fold(throw NoSuchElementException("UNone.get"))(a => a)
+    def get = self.fold(throw NoSuchElementException("UNone.get"))(a => a)
 
-extension UOptionExtractOps on [A, B >: A](self: UOption[A]):
-  def getOrElse(default: => B): B = fold[A, B](self)(default)(a => a: B)
-  def toArray(using reflect.ClassTag[B]): Array[B] = fold[A, Array[B]](self)(Array.empty)(a => Array(a))
+  extension UOptionExtractOps on [A, B >: A](self: UOption[A]):
+    def getOrElse(default: => B): B = fold[A, B](self)(default)(a => a: B)
+    def toArray(using reflect.ClassTag[B]): Array[B] = fold[A, Array[B]](self)(Array.empty)(a => Array(a))
 
-extension UOptionFlattenOps on [A <: UOption[B], B](self: UOption[A]):
-  def flatten: UOption[B] = fold[A, UOption[B]](self)(UNone)(a => a)
+  extension UOptionFlattenOps on [A <: UOption[B], B](self: UOption[A]):
+    def flatten: UOption[B] = fold[A, UOption[B]](self)(UNone)(a => a)
 
-extension UOptionNullOps on [A, B >: A | Null](self: UOption[A]):
-  def orNull: B = fold[A, B](self)(null)(a => a: B)
+  extension UOptionNullOps on [A, B >: A | Null](self: UOption[A]):
+    def orNull: B = fold[A, B](self)(null)(a => a: B)
 
-extension UOptionMapOps on [A, B](self: UOption[A]):
-  def map(f: A => B): UOption[B] = fold(self)(UNone)(a => wrap(f(a)))
-  def flatMap(f: A => UOption[B]): UOption[B] = fold(self)(UNone)(a => f(a))
+  extension UOptionMapOps on [A, B](self: UOption[A]):
+    def map(f: A => B): UOption[B] = fold(self)(UNone)(a => wrap(f(a)))
+    def flatMap(f: A => UOption[B]): UOption[B] = fold(self)(UNone)(a => f(a))
