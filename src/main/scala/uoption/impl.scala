04b2cdecf2f8d6a5e69bed4c21f8ab4d3f3bb85e
Louis Bettens <louis@bettens.info>
2020-05-27 12:01:09 +0200

    implement #2

--- src/main/scala/uoption/impl.scala
+++ src/main/scala/uoption/impl.scala
@@ -10,12 +10,12 @@ type UOption[+A] = UNone.type | USome[A]
 
 def empty[A]: UOption[A] = UNone
 
-def [A](a: A).wrap: USome[A] = a match
+inline def [A](a: A).wrap: USome[A] = a match
   case UNone => WrappedUNone(1)
   case WrappedUNone(l) => WrappedUNone(l+1)
   case a => a
 
-def [A, B](oa: UOption[A]).fold(ifEmpty: => B)(fa: A => B): B = oa match
+inline def [A, B](oa: UOption[A]).fold(inline ifEmpty: B)(fa: A => B): B = oa match
   case UNone => ifEmpty
   case WrappedUNone(1) => fa(UNone.asInstanceOf[A])
   case WrappedUNone(l) => fa(WrappedUNone(l-1).asInstanceOf[A])
