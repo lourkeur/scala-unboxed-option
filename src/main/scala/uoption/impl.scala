c9137981a0f2d464edf8a5f67f0cd22d8c80f327
Louis Bettens <louis@bettens.info>
2020-06-03 10:31:49 +0200

    Change wrapping method
    pros:
    - more general
    - more typed
    - no allocation on unwrap
    
    cons:
    - O(n) space in heap and serialized form (meh)

--- src/main/scala/uoption/impl.scala
+++ src/main/scala/uoption/impl.scala
@@ -1,9 +1,7 @@
 package uoption.impl
 
 case object UNone
-case class WrappedUNone[+A](level: Int):
-  assert(level > 0)
-  override def toString = "USome(" * level + UNone + ")" * level
+case class WrappedUNone[+A](unwrap: A)
 
 type USome[+A] = A | WrappedUNone[A]
 type UOption[+A] = UNone.type | USome[A]
@@ -11,12 +9,11 @@ type UOption[+A] = UNone.type | USome[A]
 def empty[A]: UOption[A] = UNone
 
 inline def [A](a: A).wrap: USome[A] = a match
-  case UNone => WrappedUNone(1)
-  case WrappedUNone(l) => WrappedUNone(l+1)
+  case UNone => WrappedUNone(a)
+  case a: WrappedUNone[_] => WrappedUNone(a)
   case a => a
 
 inline def [A, B](oa: UOption[A]).fold(inline ifEmpty: B)(fa: A => B): B = oa match
   case UNone => ifEmpty
-  case WrappedUNone(1) => fa(UNone.asInstanceOf[A])
-  case WrappedUNone(l) => fa(WrappedUNone(l-1).asInstanceOf[A])
+  case WrappedUNone(a) => fa(a.asInstanceOf[A])
   case a => fa(a.asInstanceOf[A])
